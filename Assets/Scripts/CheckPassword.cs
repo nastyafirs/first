567296e88064e1984575d134b738ce2a78bd21bb
dmtr <kabanov.dmitry.vladimirovich@gmail.com>
2019-12-17 20:02:53 +0300

    dgdx

---
+++ Assets/Scripts/CheckPassword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPassword : MonoBehaviour
+{
+
+    private string password = "qwe";
+    public InputField inputField;
+
+    public void OnClickStart()
+    {
+        if (password.Equals(inputField.text)){
+            Debug.Log("Password is ok");
+        }
+
+    }
+}
